19cc2022f9296fa5322c3f67e42d076bd816ee54
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-07-27 10:02:47 +0500

    3rd commit

---
+++ Ch5-P4-EmployeeApp/Employee.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch5_P4_EmployeeApp
+{
+    class Employee
+    {
+        // Field data.
+        private string empName;
+        private int empID;
+        private float currPay;
+        private int empAge;
+
+        #region Constructors
+
+
+        // Constructors.
+        public Employee() { }
+        public Employee(string name, int id, float pay) : this(name, 0, id, pay){}
+        public Employee(string name, int age, int id, float pay)
+        {
+            empName = name;
+            empID = id;
+            empAge = age;
+            currPay = pay;
+        }
+
+        #endregion
+
+        #region Properties
+
+
+        // Properties!
+        public int Age
+        {
+            get { return empAge; }
+            set => empAge = value;
+        }
+        public string Name
+        {
+            get { return empName; }
+            set
+            {
+                if (value.Length > 15)
+                    Console.WriteLine("Error! Name length exceeds 15 characters!");
+                else
+                    empName = value;
+            }
+        }
+        public int ID
+        {
+            get { return empID; }
+            set { empID = value; }
+        }
+        public float Pay
+        {
+            get { return currPay; }
+            set { currPay = value; }
+        }
+
+        #endregion
+
+        // Accessor (get method).
+        public string GetName()
+        {
+            return empName;
+        }
+        // Mutator (set method).
+        public void SetName(string name)
+        {
+            // Do a check on incoming value
+            // before making assignment.
+            if (name.Length > 15)
+                Console.WriteLine("Error! Name length exceeds 15 characters!");
+            else
+                empName = name;
+        }
+
+
+        // Methods.
+        public void GiveBonus(float amount)
+        {
+            currPay += amount;
+        }
+        public void DisplayStats()
+        {
+            Console.WriteLine("Name: {0}", empName);
+            Console.WriteLine("ID: {0}", empID);
+            Console.WriteLine("Age: {0}", empAge);
+            Console.WriteLine("Pay: {0}", currPay);
+        }
+    }
+}
--- Ch5-P4-EmployeeApp/Program.cs
+++ Ch5-P4-EmployeeApp/Program.cs
@@ -10,6 +10,47 @@ namespace Ch5_P4_EmployeeApp
     {
         static void Main(string[] args)
         {
+            //Employee emp = new Employee();
+
+            ////emp.empName = "Ahmad"; // no access
+            //emp.SetName("hello g");
+
+            #region encapsulation using traditional accessors and mutators
+
+
+            //Console.WriteLine("***** Fun with Encapsulation *****\n");
+            //Employee emp = new Employee("Marvin", 456, 30_000);
+            //emp.GiveBonus(1000);
+            //emp.DisplayStats();
+            //// Use the get/set methods to interact with the object's name.
+            //emp.SetName("Marv sdfadfjaldjfajfds");
+            //Console.WriteLine("Employee is named: {0}", emp.GetName());
+
+            #endregion
+
+            #region Encapsulation using .NET Properties
+
+            //Console.WriteLine("***** Fun with Encapsulation *****\n");
+            //Employee emp = new Employee("Marvin", 456, 30000);
+            //emp.GiveBonus(1000);
+            //emp.DisplayStats();
+            //// Reset and then get the Name property.
+            //emp.Name = "Marv";
+            //Console.WriteLine("Employee is named: {0}", emp.Name);
+
+
+
+            #endregion
+
+            #region Applying Intrinsic operators on .NET Properties
+
+            Employee joe = new Employee();
+            //joe.SetAge(joe.GetAge() + 1);
+
+            joe.Age++;
+
+
+            #endregion
 
             Console.ReadLine();
         }
